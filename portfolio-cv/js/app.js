21edc1af0185aef968d2984e49fe38eb8e50815a
Luciano Cenatiempo <luciano.cenatiempo.dev@gmail.com>
2021-10-17 11:15:14 -0300

    Agrega proyecto 4 (GifExpertApp)

--- portfolio-cv/js/app.js
+++ portfolio-cv/js/app.js
@@ -77,9 +77,13 @@ gsap.from('.proyectos_title', {scrollTrigger: {trigger: '.proyectos_title',toggl
 gsap.from('.habilidades_title', {scrollTrigger: {trigger: '.habilidades_title',toggleActions: 'restart none none none'},x: "-=500px",autoAlpha: 0 })
 gsap.from('.sobre-mi_title', {scrollTrigger: {trigger: '.sobre-mi_title', toggleActions: 'restart none none none'}, x: "-=500px", autoAlpha: 0 })
 gsap.from('.contacto_title', {scrollTrigger: {trigger: '.contacto_title', toggleActions: 'restart none none none'}, x: "-=500px", autoAlpha: 0})
+
+// Animaciones Proyectos
 gsap.from('.proyecto1', {scrollTrigger: {trigger: '.proyecto1',},x: "-=500px", autoAlpha: 0, duration: 0.8, delay: 0.5})
 gsap.from('.proyecto2', {scrollTrigger: {trigger: '.proyecto2',},x: "+=500px",autoAlpha: 0,duration: 0.8,delay: 0.5})
 gsap.from('.proyecto3', {scrollTrigger: {trigger: '.proyecto3',}, x: "-=500px", autoAlpha: 0, duration: 0.8, delay: 0.5})
+gsap.from('.proyecto4', {scrollTrigger: {trigger: '.proyecto4',},x: "+=500px",autoAlpha: 0,duration: 0.8,delay: 0.5})
+
 gsap.from('.tecnologias', {scrollTrigger: {trigger: '.tecnologias', }, x: "-=500px", autoAlpha: 0, duration: 0.8, delay: 0.5 })
 gsap.from('.blandas', {scrollTrigger: {trigger: '.blandas',},x: "+=500px",autoAlpha: 0,duration: 0.8,delay: 0.5})
 gsap.from('.sobre-mi_img', {scrollTrigger: {trigger: '.sobre-mi_img',},x: "-=500px",autoAlpha: 0,duration: 0.8,delay: 0.5})
